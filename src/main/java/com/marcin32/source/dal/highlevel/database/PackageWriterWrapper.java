0537318b37e1a9cf635f061cd3750e6f85e82409
Marcin Kubczak <marcin.kubczak@tomtom.com>
2020-10-30 12:10:10 +0100

    Removing packages without content while closing writer.

--- src/main/java/com/marcin32/source/dal/highlevel/database/PackageWriterWrapper.java
+++ src/main/java/com/marcin32/source/dal/highlevel/database/PackageWriterWrapper.java
@@ -4,6 +4,10 @@ import com.marcin32.source.base.PackageScope;
 import com.marcin32.source.model.CsvEntry;
 import com.marcin32.source.model.PackageDescriptorForWriting;
 import com.marcin32.source.utils.DeltaProcessingUtil;
+import com.marcin32.source.utils.FilesystemDal;
+
+import java.nio.file.Path;
+import java.util.Map;
 
 public class PackageWriterWrapper implements AutoCloseable {
 
@@ -33,12 +37,25 @@ public class PackageWriterWrapper implements AutoCloseable {
 
     @Override
     public void close() {
-        packageWriter.saveMetadata(packageDescriptorForWriting);
-        packageWriter.closePackage(packageDescriptorForWriting);
+        if (containsAnyRecords(packageDescriptorForWriting)) {
+            packageWriter.saveMetadata(packageDescriptorForWriting);
+            packageWriter.closePackage(packageDescriptorForWriting);
 
-        if (this.packageDescriptorForWriting.getPackageScope().equals(PackageScope.FULL_PACKAGE)) {
-            final DeltaProcessingUtil deltaProcessingUtil = new DeltaProcessingUtil(packageDescriptorForWriting);
-            deltaProcessingUtil.createDelta();
+            if (this.packageDescriptorForWriting.getPackageScope().equals(PackageScope.FULL_PACKAGE)) {
+                final DeltaProcessingUtil deltaProcessingUtil = new DeltaProcessingUtil(packageDescriptorForWriting);
+                deltaProcessingUtil.createDelta();
+            }
+        } else {
+            final Path basePathWithPackageName = packageDescriptorForWriting.getBasePathWithPackageName();
+            FilesystemDal.deleteFolder(basePathWithPackageName.toFile());
         }
+    }
+
+    private boolean containsAnyRecords(final PackageDescriptorForWriting packageDescriptorForWriting) {
+        return packageDescriptorForWriting
+                .getListOfMetadataForWriting()
+                .stream()
+                .mapToLong(Map.Entry::getValue)
+                .sum() > 0;
     }
 }
